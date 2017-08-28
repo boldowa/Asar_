72941c7c3285c94ef382ebb85112b9a0ac126d20
boldowa <boldowa.q@gmail.com>
2017-08-29 01:04:31 +0900

    make script

--- make.sh
+++ make.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+
+cmd() {
+	echo "$*"
+	$*
+}
+
+CFLAGS="-std=c++98 -Wall -Wno-unused-result -Wno-uninitialized"
+CFLAGS="$CFLAGS -fno-rtti -Dstricmp=strcasecmp -DRELEASE"
+CFLAGS="$CFLAGS -fdata-sections -ffunction-sections -Wl,--gc-sections"
+
 #For all Linux users out there. I don't think anything uses the Asar library on Linux, but it does no harm.
-g++ -std=c++98 -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -oasar -g -fno-rtti -DINTERFACE_CLI -Dstricmp=strcasecmp -Dlinux -DRELEASE
-g++ -fPIC -std=c++98 -fPIC -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -olibasar.so -g -fno-rtti -DINTERFACE_LIB -Dstricmp=strcasecmp -Dlinux -DRELEASE -shared
+CXX=g++
+cmd $CXX -Dlinux -s -O2 -oasar -DINTERFACE_CLI $CFLAGS *.cpp
+cmd $CXX -Dlinux -s -O2 -shared -fpic -olibasar.so -DINTERFACE_LIB $CFLAGS *.cpp
+
+#Win32
+CXX=i686-w64-mingw32-g++
+cmd $CXX -D_WIN32 -static -s -O2 -oasar.exe -DINTERFACE_CLI $CFLAGS *.cpp
+cmd $CXX -D_WIN32 -static -s -O2 -shared -fpic -oasar.dll -DINTERFACE_LIB $CFLAGS *.cpp
